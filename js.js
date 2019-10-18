5d5d199d7ccab793785f85ca56c6b28eb2ebe488
RaulZoidze <54436786+RaulZoidze@users.noreply.github.com>
2019-10-18 18:32:06 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,23 +2,21 @@
 
 let  money = 22000, 
      income = "Фриланс", 
-     addExpenses = "Квартира,машина,кот".toLowerCase(), 
+     addExpenses = "Квартира,машина,кот".toLowerCase(),
      deposit = true, 
      mission = 500000, 
-     period = 6 + " месяцев",
+     period = 6 ,
      budgetDay = money / 30;
 
 console.log(typeof money );
 console.log(typeof income);
 console.log(typeof deposit);
 console.log (income.length);
-console.log("Период " + period );
+console.log("Период " + period + " месяцев");
 console.log("Цель заработать " + mission + " долларов");
-console.log(addExpenses.split(' '));
+console.log(addExpenses.split(','));
 console.log(budgetDay + " Дневной Бюджет");
 console.log(budgetDay % 30 + " Остаток от деления");
-
-
 
 
 
