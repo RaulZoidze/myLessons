bc02ffad089f7dc5dbe0407bc9d5a5767a1b622b
RaulZoidze <Raulzoidze3301@gmail.com>
2019-10-18 11:15:51 +0300

    i create lesson_02

--- js.js
+++ js.js
@@ -1,11 +1,25 @@
 "use strict"
 
-let  money, 
-     income, 
-     addExpenses, 
-     deposit, 
-     mission, 
-     period;
+let  money = 22000, 
+     income = "Фриланс", 
+     addExpenses = "Квартира,машина,кот".toLowerCase(), 
+     deposit = true, 
+     mission = 500000, 
+     period = 6 + " месяцев",
+     budgetDay = money / 30;
 
-alert("Hello Glo Academy");
-console.log("Cicada3301");
+console.log(typeof money );
+console.log(typeof income);
+console.log(typeof deposit);
+console.log (income.length);
+console.log("Период " + period );
+console.log("Цель заработать " + mission + " долларов");
+console.log(addExpenses.split(' '));
+console.log(budgetDay + " Дневной Бюджет");
+console.log(budgetDay % 30 + " Остаток от деления");
+
+
+
+
+
+
