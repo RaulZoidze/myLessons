b5b9a2ff7dfe17d4071c29b4b10d6711c586494d
RaulZoidze <54436786+RaulZoidze@users.noreply.github.com>
2019-10-22 00:55:00 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,30 +1,45 @@
 "use strict";
 
-let  money = prompt (" Ваш месячный доход? " , 22000) , 
-     income = "Фриланс", 
+ 
+ 
+  
+let  money;
+
+  while (isNaN(money) || money === '' || money === null){
+    money = prompt("Ваш месячный доход?", 23000);
+     };
+
+let  income = "Фриланс", 
      addExpenses = prompt (" Перечислите возможные расходы за рассчитываемый период через запятую "),      
      deposit = confirm("Есть ли у вас депозит в банке?"), 
-     costsFirst1= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ,
-     costsFirst2= prompt ("Во сколько это обойдется?") ,
-     costsFirst3= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ,
-     costsFirst4= prompt ("Во сколько это обойдется?") ,
-     mission = 1000000, 
+     costsFirst1= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ;
+let  costsFirst2; 
+while (isNaN(costsFirst2) || costsFirst2 === '' || costsFirst2 === null){
+    costsFirst2 = prompt("Во сколько это обойдется?", 1000);
+     };
+let  costsFirst3= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ,
+     costsFirst4;
+while (isNaN(costsFirst4) || costsFirst4 === '' || costsFirst4 === null){
+    costsFirst4 = prompt("Во сколько это обойдется?", 1000);
+     };
+let  mission = 1000000, 
      period = 6 ,
      budgetMonth = money - costsFirst2 - costsFirst4,
      budgetDay = budgetMonth / 30,
      output;
 
-console.log(typeof money );
+     
+console.log(typeof +money );
 console.log(typeof income);
 console.log(typeof deposit);
 console.log (income.length);
 console.log("Период " + period + " месяцев");
 console.log("Цель заработать " + mission + " долларов");
-console.log(addExpenses.split(','));
+console.log(addExpenses.toLowerCase().split(','));
 console.log(budgetDay % 30 + " Остаток от деления");
 console.log(budgetMonth + ' Месячный Бюджет');
 console.log(Math.ceil((mission / budgetMonth)) + " месяцев до цели ");  
-console.log(budgetDay + " Дневной Бюджет");        
+console.log("Дневной Бюджет " + Math.floor(budgetDay));        
 
 if   (budgetDay >= 800){  
         output = "Высокий уровень дохода"  ;     
