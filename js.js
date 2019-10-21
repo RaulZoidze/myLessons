8c6a572face7d771c8b60c433863dc970c13c18a
RaulZoidze <Raulzoidze3301@gmail.com>
2019-10-21 21:05:22 +0300

    i create lesson_03

--- js.js
+++ js.js
@@ -1,25 +1,41 @@
-"use strict"
+"use strict";
 
-let  money = 22000, 
+let  money = prompt (" Ваш месячный доход? " , 22000) , 
      income = "Фриланс", 
-     addExpenses = "Квартира,машина,кот".toLowerCase(), 
-     deposit = true, 
-     mission = 500000, 
-     period = 6 + " месяцев",
-     budgetDay = money / 30;
+     addExpenses = prompt (" Перечислите возможные расходы за рассчитываемый период через запятую "),      
+     deposit = confirm("Есть ли у вас депозит в банке?"), 
+     costsFirst1= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ,
+     costsFirst2= prompt ("Во сколько это обойдется?") ,
+     costsFirst3= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ,
+     costsFirst4= prompt ("Во сколько это обойдется?") ,
+     mission = 1000000, 
+     period = 6 ,
+     budgetMonth = money - costsFirst2 - costsFirst4,
+     budgetDay = budgetMonth / 30,
+     output;
 
 console.log(typeof money );
 console.log(typeof income);
 console.log(typeof deposit);
 console.log (income.length);
-console.log("Период " + period );
+console.log("Период " + period + " месяцев");
 console.log("Цель заработать " + mission + " долларов");
-console.log(addExpenses.split(' '));
-console.log(budgetDay + " Дневной Бюджет");
+console.log(addExpenses.split(','));
 console.log(budgetDay % 30 + " Остаток от деления");
+console.log(budgetMonth + ' Месячный Бюджет');
+console.log(Math.ceil((mission / budgetMonth)) + " месяцев до цели ");  
+console.log(budgetDay + " Дневной Бюджет");        
 
-
-
-
-
-
+if   (budgetDay >= 800){  
+        output = "Высокий уровень дохода"  ;     
+    }
+else if (budgetDay >= 300 && budgetDay < 800 ) {
+        output = "Средний уровень дохода" ;
+    }
+else if (budgetDay >= 0 && budgetDay < 300) {
+        output = "Низкий уровень дохода" ;
+    }
+else  {
+        output = "Что то пошло не так";
+    }
+         console.log(output);
