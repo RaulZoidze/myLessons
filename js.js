2d1db904638c949ee349accea62f549287e4b75f
RaulZoidze <54436786+RaulZoidze@users.noreply.github.com>
2019-10-31 02:27:26 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,43 +1,50 @@
 'use strict'
 //перемещение книг
-let books = document.querySelectorAll('.books'),
-    book = document.querySelectorAll('.book');
+let books = document.querySelectorAll('.books'),                /////
+    book = document.querySelectorAll('.book');  
+    console.log(book);
+                /////
 
-books[0].insertBefore(book[0], book[2]);
-books[0].insertBefore(book[4], book[2]);
-books[0].insertBefore(book[2], book[5]);
-books[0].insertBefore(book[5], book[2]);
+books[0].insertBefore(book[0], book[2]);                /////
+books[0].insertBefore(book[4], book[2]);                /////
+books[0].insertBefore(book[2], book[5]);                /////
+books[0].insertBefore(book[5], book[2]);                /////
  
 //Меняем фон
-let fon = document.querySelector('body');
-fon.setAttribute('style', 'background-image: url(./image/adv.jpg)');
+let back = document.querySelector('body');
+back.setAttribute('style', 'background-image: url(./image/adv.jpg)');               /////
 
 //книга 3
-let elem3 = document.querySelectorAll('a');
-elem3[2].textContent = 'Книга 3. this и Прототипы Объектов'
-//реклама!
-let noAdv = document.querySelectorAll('body'),
-    chilAdv = document.querySelectorAll('div');
-noAdv[0].removeChild(chilAdv[6]);
-//восстановление порядка в главах и во всем мире в перспективе 
-let chapter = document.querySelectorAll('ul'),
-    level = document.querySelectorAll('li');
+let elem3 = document.querySelectorAll('a');             /////
+elem3[2].textContent = 'Книга 3. this и Прототипы Объектов'             /////
+//реклама!              /////
+let noAdv = document.querySelectorAll('body'),              /////
+    chilAdv = document.querySelectorAll('div');             /////
+noAdv[0].removeChild(chilAdv[6]);               /////
+//восстановление порядка в главах и во всем мире в перспективе     
 //книга 2
-chapter[1].insertBefore(level[12],level[10]);
-chapter[1].insertBefore(level[14],level[10]);
-chapter[1].insertBefore(level[8],level[16]);
-console.log(chapter,level);
+let ulBook2 = book[0].querySelector('ul'),              
+    liBook2 = ulBook2.querySelectorAll('li');   
+
+ulBook2.insertBefore(liBook2[2],liBook2[10]);
+ulBook2.insertBefore(liBook2[6],liBook2[4]);
+ulBook2.insertBefore(liBook2[8],liBook2[4]);
 //книга 5
-chapter[4].insertBefore(level[45],level[38]);
-chapter[4].insertBefore(level[39],level[38]);
-chapter[4].insertBefore(level[40],level[38]);
-chapter[4].insertBefore(level[41],level[44]);
+let ulBook5 = book[5].querySelector('ul'),
+    liBook5 = ulBook5.querySelectorAll('li');
+
+ulBook5.insertBefore(liBook5[9],liBook5[2]);
+ulBook5.insertBefore(liBook5[3],liBook5[2]);
+ulBook5.insertBefore(liBook5[4],liBook5[2]);
+ulBook5.insertBefore(liBook5[5],liBook5[8]);
 //книга 6
-//добавляем главу книге 6
-let clone = level[56].cloneNode();
-    chapter[5].appendChild(clone).textContent='Глава 8: За пределами ES6';
-//....
-   
+let ulBook6 = book[2].querySelector('ul'),
+    liBook6 = ulBook6.querySelectorAll('li'),
+    clone = liBook6[8].cloneNode();
+//добавил Ли
+liBook6[8].appendChild(clone).textContent= 'Глава 8: За пределами ES6';
+console.log( liBook6);
+
 
     
 
